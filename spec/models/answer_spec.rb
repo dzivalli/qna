880f3a0428afb5c887b30d64530ac1be4b1c40ed
Stas <dzivalli@ya.ru>
2015-03-11 08:46:10 +0300

    Add question and answer models

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  context 'associations' do
+    it { is_expected.to belong_to :question}
+  end
+  
+end
