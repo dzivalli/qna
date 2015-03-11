2b056ed5fd5fbd86cf71bfa11a308236b10ee19a
Stas <dzivalli@ya.ru>
2015-03-11 20:32:49 +0300

    Add question controller and tests

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,146 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 4) }
+
+    before do
+      get :index
+    end
+
+    it 'should return all questions' do
+      expect(assigns(:questions)).to match_array questions
+    end
+
+    it 'should render index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before do
+      get :show, id: question
+    end
+
+    it 'should return question by its id' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'should render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      get :new
+    end
+
+    it 'should return a new question' do
+      expect(assigns(:question)).to be_a_new Question
+    end
+
+    it 'should render new template' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'should create new questions' do
+        expect { post :create, question: attributes_for(:question) }
+            .to change(Question, :count).by 1
+      end
+
+      it 'should redirect to the new question path' do
+        post :create, question: attributes_for(:question)
+
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid params' do
+      it 'should not create question' do
+        expect { post :create, question: { body:nil, title: nil} }
+            .to_not change(Question, :count)
+      end
+
+      it 'should re-render new template' do
+        post :create, question: { body:nil, title: nil}
+
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:question) { create(:question) }
+
+    before do
+      get :edit, id: question
+    end
+
+    it 'should return question by id' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'should render edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid params' do
+      let(:question) { create(:question) }
+
+      before do
+        patch :update, id: question, question: {body: 'aaa', title: 'bbb'}
+      end
+
+      it 'should update the question by id' do
+        expect(assigns(:question).body).to eq 'aaa'
+        expect(assigns(:question).title).to eq 'bbb'
+      end
+
+      it 'should redirect to question path' do
+        expect(response).to redirect_to question_path(question)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:question) { create(:question, body: 'aaa', title: 'bbb')}
+
+      before do
+        patch :update, id: question, question: {body: 'www', title: nil}
+      end
+
+      it 'should do not update question' do
+        question.reload
+        expect(question.body).to_not eq 'www'
+        expect(question.title).to eq 'bbb'
+      end
+
+      it 'should re-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:question) { create(:question) }
+
+    it 'should delete question by its id' do
+      question
+      expect { delete :destroy, id: question }.to change(Question, :count).by -1
+    end
+
+    it 'should redirect to index page' do
+      delete :destroy, id: question
+
+      expect(response).to redirect_to questions_path
+    end
+  end
+end
