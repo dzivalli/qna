1be9da5e5840080df05b2ff56e4e6a4264582e7c
Stas <dzivalli@ya.ru>
2015-03-13 15:14:39 +0300

    Add answer controller and tests

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,49 @@
+class AnswersController < ApplicationController
+  before_action :find_question
+  before_action :find_answer, only: [:edit, :update, :destroy]
+
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new answer_params.merge(question: @question)
+    if @answer.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @answer.update answer_params
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @answer.destroy
+      redirect_to @question
+    end
+  end
+
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def find_question
+    @question = Question.find params[:question_id]
+  end
+
+  def find_answer
+    @answer = Answer.find params[:id]
+  end
+end
