6b82367142f867d8b04f2ba0d6f5134dbe452a9a
Stas <dzivalli@ya.ru>
2015-05-10 20:54:20 +0300

    Add doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { sessions: "users/sessions",
                                     registrations: "users/registrations",
                                     omniauth_callbacks: "users/omniauth_callbacks" }
