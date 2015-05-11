23baa048183ab7a69bb14a35a063c42da9980392
Stas <dzivalli@ya.ru>
2015-05-11 10:45:38 +0300

    Set doorkeeper factories

---
+++ spec/factories/oauth_applications.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :oauth_application, class: Doorkeeper::Application do
+    name 'test'
+    secret 'secret'
+    uid '1'
+    redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
+  end
+end
