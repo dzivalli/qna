1456ba365bc4381375a2fbf0364d3d891b516752
Stas <dzivalli@ya.ru>
2015-05-21 20:32:07 +0300

    Set rails env for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.4.0'
 
 set :application, 'qna'
 set :repo_url, 'git@github.com:dzivalli/qna.git'
+set :rails_env, :production
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deployer/qna'
@@ -12,7 +13,7 @@ set :deploy_to, '/home/deployer/qna'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env', 'config/private_pub.yml', 'puma.rb')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
