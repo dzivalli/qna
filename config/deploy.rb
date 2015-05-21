dbd60bc92ce3b4e5167588858f6690a3f23758a5
Stas <dzivalli@ya.ru>
2015-05-21 20:36:58 +0300

    Add private pub task to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,5 +28,39 @@ namespace :deploy do
       # end
     end
   end
-
 end
+
+# private pub tasks
+set :private_pub_pid, -> { "#{current_path}/tmp/pids/private_pub.pid" }
+
+namespace :private_pub do
+  desc "Start private_pub server"
+  task :start do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:stage) do
+          execute :bundle, "exec thin -C config/private_pub/thin_#{fetch(:stage)}.yml -d -P #{fetch(:private_pub_pid)} start"
+        end
+      end
+    end
+  end
+
+  desc "Stop private_pub server"
+  task :stop do
+    on roles(:app) do
+      within release_path do
+        execute "if [ -f #{fetch(:private_pub_pid)} ] && [ -e /proc/$(cat #{fetch(:private_pub_pid)}) ]; then kill -9 `cat #{fetch(:private_pub_pid)}`; fi"
+      end
+    end
+  end
+
+  desc "Restart private_pub server"
+  task :restart do
+    on roles(:app) do
+      invoke 'private_pub:stop'
+      invoke 'private_pub:start'
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
