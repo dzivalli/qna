a15702da41d110ff09fe6a2b7f0410305999f3ed
Stas <dzivalli@ya.ru>
2015-05-31 15:00:08 +0300

    Add caching to question#show

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ActiveRecord::Base
   include Commentable
   include Reputable
 
-  belongs_to :question
+  belongs_to :question, touch: true
   belongs_to :user
 
   validates :body, presence: true
